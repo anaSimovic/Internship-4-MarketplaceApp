040bcf385f476360ecee816a1eb5ace6d53f2ded
anaSimovic <anasimovic092@gmail.com>
2024-11-29 11:27:50 +0100

    Product.cs and User.cs

--- MarketplaceApp/Marketplace.Domain/Product.cs
+++ MarketplaceApp/Marketplace.Domain/Product.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Marketplace.Domain
+﻿namespace Marketplace.Domain
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; } = "For Sale";
+        public string Category { get; set; }
+        public Seller Seller { get; set; }
+
+        public Product(int id, string name, string description, decimal price, string category, Seller seller)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Category = category;
+            Seller = seller;
+        }
     }
 }
--- MarketplaceApp/Marketplace.Domain/User.cs
+++ MarketplaceApp/Marketplace.Domain/User.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Marketplace.Domain
+﻿namespace Marketplace.Domain
 {
-    internal class User
+    public abstract class User
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        protected User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+
+    public class Buyer : User
+    {
+        public decimal Balance { get; set; }
+        public List<Product> PurchaseHistory { get; } = new();
+        public List<Product> Favorites { get; } = new();
+
+        public Buyer(string name, string email, decimal balance)
+            : base(name, email)
+        {
+            Balance = balance;
+        }
+    }
+
+    public class Seller : User
+    {
+        public List<Product> Products { get; } = new();
+        public decimal TotalEarnings { get; set; }
+
+        public Seller(string name, string email)
+            : base(name, email) { }
     }
 }
